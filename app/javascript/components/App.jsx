063fcec9e5412572201cc78db6089d6db537b71f
twknab <20636750+twknab@users.noreply.github.com>
2023-02-15 21:02:15 -0800

    wire up to react

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const App = () => {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/user")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div>
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user.email}>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
