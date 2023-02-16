d0b8487a36bc466ac54487218d433d5c157ffe79
twknab <20636750+twknab@users.noreply.github.com>
2023-02-15 20:02:15 -0800

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "homepage#index"
 end
