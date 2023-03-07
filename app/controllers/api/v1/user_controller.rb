d773476d5b6de00b9aa67fbbf44d5b8560a45758
twknab <20636750+twknab@users.noreply.github.com>
2023-03-06 19:59:25 -0800

    remove unneeded param

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -44,6 +44,6 @@ class Api::V1::UserController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :password_digest)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
