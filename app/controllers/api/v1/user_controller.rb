dbfb4b6bea393e076eab7fce3b6d04c2cb19d4e5
twknab <20636750+twknab@users.noreply.github.com>
2023-03-06 19:54:06 -0800

    add password_digest

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -44,6 +44,6 @@ class Api::V1::UserController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :password_digest)
   end
 end
